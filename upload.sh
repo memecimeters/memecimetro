06217a162c091057cca5b707f757bf7eea365760
Francisco Demartino <demartino.francisco@gmail.com>
2017-05-11 02:35:44 -0300

    upload.sh: check CONFIG_MEGA and set right params

--- upload.sh
+++ upload.sh
@@ -6,13 +6,27 @@ TTY=/dev/ttyUSB0
 
 tup
 
-# asume nano
+source tup.config
+
+# chequea que NO este seteado mega
+if [ -z "$CONFIG_MEGA" ]; then
+  # nano
+  MCU=atmega328p
+  PROG=arduino
+  BAUD=57600
+else
+  # mega
+  MCU=atmega2560
+  PROG=wiring
+  BAUD=115200
+fi
+
 avrdude \
   -C ./etc/avrdude.conf \
-  -p atmega328p \
+  -p $MCU \
   -P $TTY \
-  -c arduino \
-  -b 57600 \
+  -c $PROG \
+  -b $BAUD \
   -D  \
   -U flash:w:$FIRMWARE:i
 
